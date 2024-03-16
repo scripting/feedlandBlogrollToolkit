7251cc381f58518a3a601ab3d42eae256b4703f2
Dave Winer <dave.winer@gmail.com>
2024-03-16 10:30:13 -0400

    .

--- code.js
+++ code.js
@@ -6,14 +6,10 @@ const appConsts = {
 	urlBlogrollOpml: "https://feedland.com/opml?screenname=cagrimmett&catname=blogroll",
 	urlFeedlandViewBlogroll: "https://feedland.com/?username=cagrimmett&catname=blogroll", 
 	maxItemsInBlogroll: 40,
-	flShowSocketMessages: true,
-	flBlogrollUpdates: true,
 	blogrollDisplayedCallback: function () {
 		$(".divPageBody").css ("display", "block");
 		}
 	};
 function startup () {
-	console.log ("startup");
 	blogroll (appConsts);
-	hitCounter ();
 	}
--- worknotes.md
+++ worknotes.md
@@ -1,3 +1,7 @@
+#### 3/16/24; 10:13:47 AM by DW
+
+Started the repo.
+
 #### 3/14/24; 4:58:05 PM by DW
 
 A starter kit for people embedding the blogroll interface in their templates.
